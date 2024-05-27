0e5a76c5984722eb9d63bd577046ff1a43853eb9
KSEONYT <kseonyt@outlook.com>
2024-05-27 18:13:39 +0300

    Fixed and improved bedcheck

--- src/main/java/ac/grim/grimac/checks/impl/misc/BedBreakA.java
+++ src/main/java/ac/grim/grimac/checks/impl/misc/BedBreakA.java
@@ -8,13 +8,18 @@ import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.DiggingAction;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityVelocity;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 
+@CheckData(name = "BedBreakA", experimental = false)
+public class BedBreakA extends Check implements PacketCheck {
 
-@CheckData(name = "ProtocolA", experimental = false)
-public class BedBreakA extends Check implements PacketCheck {// Define the maximum reach distance
     private int avarage, spoof;
     public static boolean velocity;
+
     public BedBreakA(GrimPlayer player) {
         super(player);
     }
@@ -22,29 +27,41 @@ public class BedBreakA extends Check implements PacketCheck {// Define the maxim
     @Override
     public void onPacketReceive(PacketReceiveEvent e) {
         if (velocity) {
-        if (e.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
-            WrapperPlayClientPlayerDigging digging = new WrapperPlayClientPlayerDigging(e);
-            if (digging.getAction()== DiggingAction.START_DIGGING) {
-                //System.out.println(player.getName() + " " + digging.getAction());
-                flagAndAlert();
-                avarage = 0;
-                velocity = false;
+            if (e.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
+                WrapperPlayClientPlayerDigging digging = new WrapperPlayClientPlayerDigging(e);
+                if (digging.getAction() == DiggingAction.START_DIGGING) {
+                    assert player.bukkitPlayer != null;
+                    Location blockLocation = new Location(player.bukkitPlayer.getWorld(), digging.getBlockPosition().getX(), digging.getBlockPosition().getY(), digging.getBlockPosition().getZ());
+                    Block block = blockLocation.getBlock();
+                    if (block.getType() == Material.RED_BED && isBedSurrounded(block)) {
+                        flagAndAlert();
+                        avarage = 0;
+                        velocity = false;
+                    }
+                }
             }
         }
-        }
         if (e.getPacketType() == PacketType.Play.Client.INTERACT_ENTITY) {
-
         }
         if (e.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION) {
-            velocity=false;
+            velocity = false;
         }
     }
+
+    private boolean isBedSurrounded(Block bedBlock) {
+        BlockFace[] faces = {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.UP, BlockFace.DOWN};
+        for (BlockFace face : faces) {
+            if (bedBlock.getRelative(face).getType() == Material.AIR) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public void reload() {
         super.reload();
         velocity = false;
         avarage = 0;
-        //getConfig().getDoubleElse("NoSlowA.threshold", 0.001);
-        //this.flagclick = getConfig().getInt("AutoClicker.maxcps");
     }
 }
